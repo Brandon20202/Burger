7da2ddbaa9033e4f19a36db8d22e4570f6505644
Brandon20202 <brandon.taylor@austinisd.org>
2020-05-02 11:41:45 -0500

    fixed code

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 var express = require("express");
 
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+
 
 var port = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+app.use(express.static("/public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -24,7 +21,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller");
 
-app.use("/", routes);
+app.use(routes);
 
 app.listen(port, function() {
     // Log (server-side) when our server has started
